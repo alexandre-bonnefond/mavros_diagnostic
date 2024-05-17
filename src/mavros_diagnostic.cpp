4f8a8c6774443f4b626c1430ff2e646c6ae0f02f
Alexandre Bonnefond <alexandre.bonnefond95@gmail.com>
2024-05-17 11:20:50 +0200

    typo + ROS_INFO to print for JSON parsing

--- src/mavros_diagnostic.cpp
+++ src/mavros_diagnostic.cpp
@@ -17,39 +17,39 @@ void callback(const diagnostic_msgs::DiagnosticArray::ConstPtr& msg)
     }
 
     if (name.find("FCU connection") != std::string::npos) {
-      ROS_INFO("FCU state: %s", message.c_str());
+      print("FCU state: %s", message.c_str());
     }
 
     if (name.find("Heartbeat") != std::string::npos) {
-      ROS_INFO("Hearbeat Frequency: %s", values["Frequency (Hz)"].c_str());
+      print("Heartbeat Frequency: %s", values["Frequency (Hz)"].c_str());
       /* auto it = values.find("Frequency"); */
       /* if (it != values.end()) { */
       /*   std::string frequency = it->second; */
-      /*   ROS_INFO("Heartbeat Frequency: %s", frequency.c_str()); */
+      /*   print("Heartbeat Frequency: %s", frequency.c_str()); */
       /* } */
     }
 
     if (name.find("System") != std::string::npos) {
       /* auto it = values["3D gyro"]; */
       /* std::cout<< it << std::endl; */
-      ROS_INFO("3D gyro: %s", values["3D gyro"].c_str());
-      ROS_INFO("3D accelerometer: %s", values["3D accelerometer"].c_str());
-      ROS_INFO("3D magnetometer: %s", values["3D magnetometer"].c_str());
-      ROS_INFO("GPS: %s", values["GPS"].c_str());
-      ROS_INFO("rc receiver: %s", values["rc receiver"].c_str());
-      ROS_INFO("AHRS: %s", values["AHRS subsystem health"].c_str());
-      ROS_INFO("Battery: %s", values["Battery"].c_str());
-      ROS_INFO("pre-arm check: %s", values["pre-arm check status. Always healthy when armed"].c_str());
-      ROS_INFO("CPU Load: %s", values["CPU Load (%)"].c_str());
+      print("3D gyro: %s", values["3D gyro"].c_str());
+      print("3D accelerometer: %s", values["3D accelerometer"].c_str());
+      print("3D magnetometer: %s", values["3D magnetometer"].c_str());
+      print("GPS: %s", values["GPS"].c_str());
+      print("rc receiver: %s", values["rc receiver"].c_str());
+      print("AHRS: %s", values["AHRS subsystem health"].c_str());
+      print("Battery: %s", values["Battery"].c_str());
+      print("pre-arm check: %s", values["pre-arm check status. Always healthy when armed"].c_str());
+      print("CPU Load: %s", values["CPU Load (%)"].c_str());
     }
 
     if (name.find("GPS") != std::string::npos) {
-      ROS_INFO("GPS Satellites Visible: %s", values["Satellites visible"].c_str());
+      print("GPS Satellites Visible: %s", values["Satellites visible"].c_str());
     }
 
     if (name.find("Battery") != std::string::npos) {
-      ROS_INFO("Voltage battery: %s", values["Voltage"].c_str());
-      ROS_INFO("Current battery: %s", values["Current"].c_str());
+      print("Voltage battery: %s", values["Voltage"].c_str());
+      print("Current battery: %s", values["Current"].c_str());
     }
   }
 }
