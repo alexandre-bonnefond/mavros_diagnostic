c594d39bc31a69dc170a9a81b8765612fc171778
Alexandre Bonnefond <alexandre.bonnefond95@gmail.com>
2024-05-17 15:28:16 +0200

    printf...

--- src/mavros_diagnostic.cpp
+++ src/mavros_diagnostic.cpp
@@ -17,39 +17,39 @@ void callback(const diagnostic_msgs::DiagnosticArray::ConstPtr& msg)
     }
 
     if (name.find("FCU connection") != std::string::npos) {
-      print("FCU state: %s", message.c_str());
+      printf("FCU state: %s", message.c_str());
     }
 
     if (name.find("Heartbeat") != std::string::npos) {
-      print("Heartbeat Frequency: %s", values["Frequency (Hz)"].c_str());
+      printf("Heartbeat Frequency: %s", values["Frequency (Hz)"].c_str());
       /* auto it = values.find("Frequency"); */
       /* if (it != values.end()) { */
       /*   std::string frequency = it->second; */
-      /*   print("Heartbeat Frequency: %s", frequency.c_str()); */
+      /*   printf("Heartbeat Frequency: %s", frequency.c_str()); */
       /* } */
     }
 
     if (name.find("System") != std::string::npos) {
       /* auto it = values["3D gyro"]; */
       /* std::cout<< it << std::endl; */
-      print("3D gyro: %s", values["3D gyro"].c_str());
-      print("3D accelerometer: %s", values["3D accelerometer"].c_str());
-      print("3D magnetometer: %s", values["3D magnetometer"].c_str());
-      print("GPS: %s", values["GPS"].c_str());
-      print("rc receiver: %s", values["rc receiver"].c_str());
-      print("AHRS: %s", values["AHRS subsystem health"].c_str());
-      print("Battery: %s", values["Battery"].c_str());
-      print("pre-arm check: %s", values["pre-arm check status. Always healthy when armed"].c_str());
-      print("CPU Load: %s", values["CPU Load (%)"].c_str());
+      printf("3D gyro: %s", values["3D gyro"].c_str());
+      printf("3D accelerometer: %s", values["3D accelerometer"].c_str());
+      printf("3D magnetometer: %s", values["3D magnetometer"].c_str());
+      printf("GPS: %s", values["GPS"].c_str());
+      printf("rc receiver: %s", values["rc receiver"].c_str());
+      printf("AHRS: %s", values["AHRS subsystem health"].c_str());
+      printf("Battery: %s", values["Battery"].c_str());
+      printf("pre-arm check: %s", values["pre-arm check status. Always healthy when armed"].c_str());
+      printf("CPU Load: %s", values["CPU Load (%)"].c_str());
     }
 
     if (name.find("GPS") != std::string::npos) {
-      print("GPS Satellites Visible: %s", values["Satellites visible"].c_str());
+      printf("GPS Satellites Visible: %s", values["Satellites visible"].c_str());
     }
 
     if (name.find("Battery") != std::string::npos) {
-      print("Voltage battery: %s", values["Voltage"].c_str());
-      print("Current battery: %s", values["Current"].c_str());
+      printf("Voltage battery: %s", values["Voltage"].c_str());
+      printf("Current battery: %s", values["Current"].c_str());
     }
   }
 }
